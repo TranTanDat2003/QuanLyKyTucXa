6ea1d94a7f0b852bd46c4ccec3fa739c42fc0b2f
Tran Tan Dat <trantandat10122003@gmail.com>
2025-03-02 14:14:30 +0700

    Create controller AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only('username', 'password');
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            $user = Auth::user();
+            if ($user->role === 'student') {
+                return redirect()->intended('/');
+            } elseif (in_array($user->role, ['admin', 'staff'])) {
+                return redirect()->intended('/admin');
+            }
+        }
+        return back()->withErrors(['username' => 'Thông tin đăng nhập không đúng!']);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login');
+    }
 }

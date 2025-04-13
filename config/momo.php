07af7ec7ec5b1df3ab9e25ea57142fa62e75e5b6
Tran Tan Dat <trantandat10122003@gmail.com>
2025-04-14 02:25:27 +0700

    update chuc nang thanh toan cho vnPay ZaloPay momo

---
+++ config/momo.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'endpoint' => env('MOMO_ENDPOINT'),
+    'partner_code' => env('MOMO_PARTNER_CODE'),
+    'access_key' => env('MOMO_ACCESS_KEY'),
+    'secret_key' => env('MOMO_SECRET_KEY'),
+    'redirect_url' => env('MOMO_REDIRECT_URL'),
+    'ipn_Url' => env('MOMO_NOTIFY_URL'),
+];
